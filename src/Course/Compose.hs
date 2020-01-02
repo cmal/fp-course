37939a939a1c5772fbea24291dddeda7b2996b25
Yu Zhao <yu.zhao10@ele.me>
2020-01-02 21:45:37 +0800

    feat: Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,17 +16,38 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) f (Compose a) = Compose $ ((f <$>) <$>) a
+    -- error "todo: Course.Compose (<$>)#instance (Compose f g)"
+
+
+
+-- (<$>) :: Functor f => (a -> b) -> f a -> f b
+-- Functor Laws:
+-- fmap id = id
+-- fmap (g . h) = (fmap g) . (fmap h)
+
+
+
+
+
+-- Applicative Laws:
+-- pure id <*> v = v
+-- pure f <*> pure x = pure (f x)
+-- u <*> pure y = pure ($ y) <*> u
+-- u <*> (v <*> w) = pure (.) <*> u <*> v <*> w
+
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure x =
+    -- error "todo: Course.Compose pure#instance (Compose f g)"
+    Compose $ pure $ pure x
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (<*>) (Compose x) (Compose y) =
+    -- error "todo: Course.Compose (<*>)#instance (Compose f g)"
+    Compose $ (lift2 . lift2) ($) x y
+  -- (<*>) = _todo
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
